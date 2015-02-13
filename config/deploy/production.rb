f7c148c8bce2e38dcc55fde920b03c2e10dca625
Nicolas Mery <njmery@gmail.com>
2015-02-13 10:03:58 -0500

    deploy setup for prod (its a test)

---
+++ config/deploy/production.rb
@@ -0,0 +1,13 @@
+# PRODUCTION CONFIGURATION
+
+# Servers and their roles.
+server 'kross.platan.us', user: 'deploy', roles: %w{web app db}, my_property: :my_value
+
+# Web server configuration
+set :nginx_domains, 'hello.kross.platan.us'
+
+# Source
+set :branch,        'master'    # Optional, defaults to master
+
+# Rails configuration
+# set :rails_env,   'production'
